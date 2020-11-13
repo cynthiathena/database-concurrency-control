e27dcef8aa61fe8c972164f03384aaad4a961508
cynthiathena <13518059@std.stei.itb.ac.id>
2020-11-13 09:26:45 +0700

    add files

---
+++ txn/mvcc_storage.cc
@@ -0,0 +1,164 @@
+// Author: Kun Ren (kun.ren@yale.edu)
+// Modified by Daniel Abadi
+
+#include "txn/mvcc_storage.h"
+
+// Init the storage
+void MVCCStorage::InitStorage() {
+  for (int i = 0; i < 1000000;i++) {
+    Write(i, 0, 0);
+    Mutex* key_mutex = new Mutex();
+    mutexs_[i] = key_mutex;
+    // std::cout << "inside loop" << std::endl << std::flush;
+
+  }
+  // std::cout << "Test init storage" << std::endl << std::flush;
+}
+
+// Free memory.
+MVCCStorage::~MVCCStorage() {
+  for (unordered_map<Key, deque<Version*>*>::iterator it = mvcc_data_.begin();
+       it != mvcc_data_.end(); ++it) {
+    delete it->second;          
+  }
+  
+  mvcc_data_.clear();
+  
+  for (unordered_map<Key, Mutex*>::iterator it = mutexs_.begin();
+       it != mutexs_.end(); ++it) {
+    delete it->second;          
+  }
+  
+  mutexs_.clear();
+}
+
+// Lock the key to protect its version_list. Remember to lock the key when you read/update the version_list 
+void MVCCStorage::Lock(Key key) {
+  mutexs_[key]->Lock();
+}
+
+// Unlock the key.
+void MVCCStorage::Unlock(Key key) {
+  mutexs_[key]->Unlock();
+}
+
+// MVCC Read
+bool MVCCStorage::Read(Key key, Value* result, int txn_unique_id) {
+  // CPSC 438/538:
+  //
+  // Implement this method!
+  
+  // Hint: Iterate the version_lists and return the verion whose write timestamp
+  // (version_id) is the largest write timestamp less than or equal to txn_unique_id.
+  bool found = false;
+  if (mvcc_data_.count(key)){ 
+    deque<Version*> versions = *mvcc_data_[key];
+    int temp = 0;
+    for (deque<Version*>::iterator it=versions.begin(); it!=versions.end(); ++it){
+      if (((*it)->version_id_ > temp) && ((*it)->version_id_<=txn_unique_id)){
+        temp = (*it)->version_id_;
+        *result = (*it)->value_;
+        found = true;
+        if ((*it)->max_read_id_ < txn_unique_id) {
+          (*it)->max_read_id_ = txn_unique_id;
+        }
+      }
+    }
+
+  }
+  // std::cout << "Read found : " << found << std::endl; 
+  return found;
+}
+
+
+// Check whether apply or abort the write
+bool MVCCStorage::CheckWrite(Key key, int txn_unique_id) {
+  // CPSC 438/538:
+  //
+  // Implement this method!
+  
+  // Hint: Before all writes are applied, we need to make sure that each write
+  // can be safely applied based on MVCC timestamp ordering protocol. This method
+  // only checks one key, so you should call this method for each key in the
+  // write_set. Return true if this key passes the check, return false if not. 
+  // Note that you don't have to call Lock(key) in this method, just
+  // call Lock(key) before you call this method and call Unlock(key) afterward.
+  if (mvcc_data_.count(key)){
+    deque<Version*> versions = *mvcc_data_[key];
+    int temp_write = -1;
+    int temp_read = -1;
+    for (deque<Version*>::iterator it=versions.begin(); it!=versions.end(); ++it){
+      if (((*it)->version_id_ > temp_write) && ((*it)->version_id_<=txn_unique_id)){
+        temp_write = (*it)->version_id_;
+        temp_read = (*it)->max_read_id_;
+      }
+    }
+    if (temp_read > txn_unique_id){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+  else {
+    return true;
+  }
+
+}
+
+// MVCC Write, call this method only if CheckWrite return true.
+void MVCCStorage::Write(Key key, Value value, int txn_unique_id) {
+  // CPSC 438/538:
+  //
+  // Implement this method!
+  
+  // Hint: Insert a new version (malloc a Version and specify its value/version_id/max_read_id)
+  // into the version_lists. Note that InitStorage() also calls this method to init storage. 
+  // Note that you don't have to call Lock(key) in this method, just
+  // call Lock(key) before you call this method and call Unlock(key) afterward.
+  // std::cout << "Test write" << std::endl << std::flush;
+  if (!mvcc_data_.count(key)){
+    // std::cout << "Test write no key exist in mvcc data" << std::endl << std::flush;
+    deque<Version*>* versions = new deque<Version*>;
+    mvcc_data_[key] = versions;
+    // std::cout << "Test write add key and value" << std::endl << std::flush;
+
+    Version *new_ver = new Version;
+    (new_ver)->value_ = value;
+    (new_ver)->max_read_id_ = txn_unique_id;
+    (new_ver)->version_id_ = txn_unique_id;
+    mvcc_data_[key]->push_back(new_ver);
+    // std::cout << mvcc_data_[key]->size() << std::endl << std::flush;
+    }
+  else {
+    deque<Version*> versions = *mvcc_data_[key];
+    // if (!versions.empty()){/
+      int temp = -1;
+      for (deque<Version*>::iterator it=versions.begin(); it!=versions.end(); ++it){
+        if (((*it)->version_id_ > temp) && ((*it)->version_id_<=txn_unique_id)){
+          temp = (*it)->version_id_;
+        }
+      }
+      for (deque<Version*>::iterator it=versions.begin(); it!=versions.end(); ++it){
+        if (temp == (*it)->version_id_){
+          if (txn_unique_id == (*it)->version_id_){
+            (*it)->value_ = value;
+            (*it)->max_read_id_ = txn_unique_id;
+            (*it)->version_id_ = txn_unique_id;
+          }
+          else{
+            Version *new_ver = new Version;
+            new_ver->value_ = value;
+            new_ver->max_read_id_ = txn_unique_id;
+            new_ver->version_id_ = txn_unique_id;
+            mvcc_data_[key]->push_back(new_ver);
+          }
+        }
+      // }
+    }
+
+  }
+
+}
+
+
---
+++ txn/txn_processor.cc
@@ -0,0 +1,466 @@
+// Author: Alexander Thomson (thomson@cs.yale.edu)
+// Modified by: Christina Wallin (christina.wallin@yale.edu)
+// Modified by: Kun Ren (kun.ren@yale.edu)
+
+
+#include "txn/txn_processor.h"
+#include <stdio.h>
+#include <set>
+
+#include "txn/lock_manager.h"
+
+// Thread & queue counts for StaticThreadPool initialization.
+#define THREAD_COUNT 8
+
+TxnProcessor::TxnProcessor(CCMode mode)
+    : mode_(mode), tp_(THREAD_COUNT), next_unique_id_(1) {
+  if (mode_ == LOCKING_EXCLUSIVE_ONLY)
+    lm_ = new LockManagerA(&ready_txns_);
+  else if (mode_ == LOCKING)
+    lm_ = new LockManagerB(&ready_txns_);
+
+  // Create the storage
+  if (mode_ == MVCC) {
+    storage_ = new MVCCStorage();
+  } else {
+    storage_ = new Storage();
+  }
+  // std::cout << "Test TxnProcessor" << std::endl << std::flush;
+  storage_->InitStorage();
+
+  // std::cout << "Test CPUSET" << std::endl << std::flush;
+
+  // Start 'RunScheduler()' running.
+  cpu_set_t cpuset;
+  pthread_attr_t attr;
+  pthread_attr_init(&attr);
+  // std::cout << "Test CPUSET" << std::endl << std::flush;
+  CPU_ZERO(&cpuset);
+  CPU_SET(0, &cpuset);
+  CPU_SET(1, &cpuset);
+  CPU_SET(2, &cpuset);
+  CPU_SET(3, &cpuset);
+  CPU_SET(4, &cpuset);
+  CPU_SET(5, &cpuset);
+  CPU_SET(6, &cpuset);
+  pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpuset);
+  pthread_t scheduler_;
+  pthread_create(&scheduler_, &attr, StartScheduler, reinterpret_cast<void*>(this));
+
+}
+
+void* TxnProcessor::StartScheduler(void * arg) {
+  reinterpret_cast<TxnProcessor *>(arg)->RunScheduler();
+  return NULL;
+}
+
+TxnProcessor::~TxnProcessor() {
+  if (mode_ == LOCKING_EXCLUSIVE_ONLY || mode_ == LOCKING)
+    delete lm_;
+
+  delete storage_;
+}
+
+void TxnProcessor::NewTxnRequest(Txn* txn) {
+  // Atomically assign the txn a new number and add it to the incoming txn
+  // requests queue.
+  mutex_.Lock();
+  txn->unique_id_ = next_unique_id_;
+  next_unique_id_++;
+  txn_requests_.Push(txn);
+  mutex_.Unlock();
+}
+
+Txn* TxnProcessor::GetTxnResult() {
+  Txn* txn;
+  while (!txn_results_.Pop(&txn)) {
+    // No result yet. Wait a bit before trying again (to reduce contention on
+    // atomic queues).
+    sleep(0.000001);
+  }
+  return txn;
+}
+
+void TxnProcessor::RunScheduler() {
+  // std::cout << "tEST" << std::flush;
+  switch (mode_) {
+    case SERIAL:                 RunSerialScheduler(); break;
+    case LOCKING:                RunLockingScheduler(); break;
+    case LOCKING_EXCLUSIVE_ONLY: RunLockingScheduler(); break;
+    case OCC:                    RunOCCScheduler(); break;
+    case P_OCC:                  RunOCCParallelScheduler(); break;
+    case MVCC:                   RunMVCCScheduler();
+  }
+}
+
+void TxnProcessor::RunSerialScheduler() {
+  Txn* txn;
+  while (tp_.Active()) {
+    // Get next txn request.
+    if (txn_requests_.Pop(&txn)) {
+      // Execute txn.
+      ExecuteTxn(txn);
+
+      // Commit/abort txn according to program logic's commit/abort decision.
+      if (txn->Status() == COMPLETED_C) {
+        ApplyWrites(txn);
+        txn->status_ = COMMITTED;
+      } else if (txn->Status() == COMPLETED_A) {
+        txn->status_ = ABORTED;
+      } else {
+        // Invalid TxnStatus!
+        DIE("Completed Txn has invalid TxnStatus: " << txn->Status());
+      }
+
+      // Return result to client.
+      txn_results_.Push(txn);
+    }
+  }
+}
+
+void TxnProcessor::RunLockingScheduler() {
+  Txn* txn;
+  while (tp_.Active()) {
+    // Start processing the next incoming transaction request.
+    if (txn_requests_.Pop(&txn)) {
+      bool blocked = false;
+      // Request read locks.
+      for (set<Key>::iterator it = txn->readset_.begin();
+           it != txn->readset_.end(); ++it) {
+        if (!lm_->ReadLock(txn, *it)) {
+          blocked = true;
+          // If readset_.size() + writeset_.size() > 1, and blocked, just abort
+          if (txn->readset_.size() + txn->writeset_.size() > 1) {
+            // Release all locks that already acquired
+            for (set<Key>::iterator it_reads = txn->readset_.begin(); true; ++it_reads) {
+              lm_->Release(txn, *it_reads);
+              if (it_reads == it) {
+                break;
+              }
+            }
+            break;
+          }
+        }
+      }
+
+      if (blocked == false) {
+        // Request write locks.
+        for (set<Key>::iterator it = txn->writeset_.begin();
+             it != txn->writeset_.end(); ++it) {
+          if (!lm_->WriteLock(txn, *it)) {
+            blocked = true;
+            // If readset_.size() + writeset_.size() > 1, and blocked, just abort
+            if (txn->readset_.size() + txn->writeset_.size() > 1) {
+              // Release all read locks that already acquired
+              for (set<Key>::iterator it_reads = txn->readset_.begin(); it_reads != txn->readset_.end(); ++it_reads) {
+                lm_->Release(txn, *it_reads);
+              }
+              // Release all write locks that already acquired
+              for (set<Key>::iterator it_writes = txn->writeset_.begin(); true; ++it_writes) {
+                lm_->Release(txn, *it_writes);
+                if (it_writes == it) {
+                  break;
+                }
+              }
+              break;
+            }
+          }
+        }
+      }
+
+      // If all read and write locks were immediately acquired, this txn is
+      // ready to be executed. Else, just restart the txn
+      if (blocked == false) {
+        ready_txns_.push_back(txn);
+      } else if (blocked == true && (txn->writeset_.size() + txn->readset_.size() > 1)){
+        mutex_.Lock();
+        txn->unique_id_ = next_unique_id_;
+        next_unique_id_++;
+        txn_requests_.Push(txn);
+        mutex_.Unlock();
+      }
+    }
+
+    // Process and commit all transactions that have finished running.
+    while (completed_txns_.Pop(&txn)) {
+      // Commit/abort txn according to program logic's commit/abort decision.
+      if (txn->Status() == COMPLETED_C) {
+        ApplyWrites(txn);
+        txn->status_ = COMMITTED;
+      } else if (txn->Status() == COMPLETED_A) {
+        txn->status_ = ABORTED;
+      } else {
+        // Invalid TxnStatus!
+        DIE("Completed Txn has invalid TxnStatus: " << txn->Status());
+      }
+
+      // Release read locks.
+      for (set<Key>::iterator it = txn->readset_.begin();
+           it != txn->readset_.end(); ++it) {
+        lm_->Release(txn, *it);
+      }
+      // Release write locks.
+      for (set<Key>::iterator it = txn->writeset_.begin();
+           it != txn->writeset_.end(); ++it) {
+        lm_->Release(txn, *it);
+      }
+
+      // Return result to client.
+      txn_results_.Push(txn);
+    }
+
+    // Start executing all transactions that have newly acquired all their
+    // locks.
+    while (ready_txns_.size()) {
+      // Get next ready txn from the queue.
+      txn = ready_txns_.front();
+      ready_txns_.pop_front();
+
+      // Start txn running in its own thread.
+      tp_.RunTask(new Method<TxnProcessor, void, Txn*>(
+            this,
+            &TxnProcessor::ExecuteTxn,
+            txn));
+
+    }
+  }
+}
+
+void TxnProcessor::ExecuteTxn(Txn* txn) {
+
+  // Get the start time
+  txn->occ_start_time_ = GetTime();
+
+  // Read everything in from readset.
+  for (set<Key>::iterator it = txn->readset_.begin();
+       it != txn->readset_.end(); ++it) {
+    // Save each read result iff record exists in storage.
+    Value result;
+    if (storage_->Read(*it, &result))
+      txn->reads_[*it] = result;
+  }
+
+  // Also read everything in from writeset.
+  for (set<Key>::iterator it = txn->writeset_.begin();
+       it != txn->writeset_.end(); ++it) {
+    // Save each read result iff record exists in storage.
+    Value result;
+    if (storage_->Read(*it, &result))
+      txn->reads_[*it] = result;
+  }
+
+  // Execute txn's program logic.
+  txn->Run();
+
+  // Hand the txn back to the RunScheduler thread.
+  completed_txns_.Push(txn);
+}
+
+void TxnProcessor::ApplyWrites(Txn* txn) {
+  // Write buffered writes out to storage.
+  for (map<Key, Value>::iterator it = txn->writes_.begin();
+       it != txn->writes_.end(); ++it) {
+    storage_->Write(it->first, it->second, txn->unique_id_);
+  }
+}
+
+/**
+ * Precondition: No storage writes are occuring during execution.
+ */
+bool TxnProcessor::OCCValidateTransaction(const Txn &txn) const {
+  // Check
+  for (auto&& key : txn.readset_) {
+    if (txn.occ_start_time_ < storage_->Timestamp(key))
+      return false;
+  }
+
+  for (auto&& key : txn.writeset_) {
+    if (txn.occ_start_time_ < storage_->Timestamp(key))
+      return false;
+  }
+
+  return true;
+}
+
+void TxnProcessor::RunOCCScheduler() {
+  // Fetch transaction requests, and immediately begin executing them.
+  while (tp_.Active()) {
+    Txn *txn;
+    if (txn_requests_.Pop(&txn)) {
+
+      // Start txn running in its own thread.
+      tp_.RunTask(new Method<TxnProcessor, void, Txn*>(
+                  this,
+                  &TxnProcessor::ExecuteTxn,
+                  txn));
+    }
+
+    // Validate completed transactions, serially
+    Txn *finished;
+    while (completed_txns_.Pop(&finished)) {
+      if (finished->Status() == COMPLETED_A) {
+        finished->status_ = ABORTED;
+      } else {
+        bool valid = OCCValidateTransaction(*finished);
+        if (!valid) {
+          // Cleanup and restart
+          finished->reads_.empty();
+          finished->writes_.empty();
+          finished->status_ = INCOMPLETE;
+
+          mutex_.Lock();
+          txn->unique_id_ = next_unique_id_;
+          next_unique_id_++;
+          txn_requests_.Push(finished);
+          mutex_.Unlock();
+        } else {
+          // Commit the transaction
+          ApplyWrites(finished);
+          txn->status_ = COMMITTED;
+        }
+      }
+
+      txn_results_.Push(finished);
+    }
+  }
+}
+
+void TxnProcessor::RunOCCParallelScheduler() {
+  // CPSC 438/538:
+  //
+  // Implement this method! Note that implementing OCC with parallel
+  // validation may need to create another method, like
+  // TxnProcessor::ExecuteTxnParallel.
+  // Note that you can use active_set_ and active_set_mutex_ we provided
+  // for you in the txn_processor.h
+  //
+  // [For now, run serial scheduler in order to make it through the test
+  // suite]
+  RunSerialScheduler();
+}
+
+void TxnProcessor::RunMVCCScheduler() {
+  // CPSC 438/538:
+  //
+  // Implement this method!
+
+  // Hint:Pop a txn from txn_requests_, and pass it to a thread to execute.
+  // Note that you may need to create another execute method, like TxnProcessor::MVCCExecuteTxn.
+  //
+  // [For now, run serial scheduler in order to make it through the test
+  // suite]
+  // std::cout << "RunMVCCScheduler" << std::endl << std::flush;
+  // int count = 0;
+  while (tp_.Active()){
+    Txn *txn;
+    // count += 1;
+    if (txn_requests_.Pop(&txn)) {
+
+      // std::cout << "Runtask : " << count << std::endl << std::flush;
+      // Start txn running in its own thread.
+      tp_.RunTask(new Method<TxnProcessor, void, Txn*>(
+                  this,
+                  &TxnProcessor::MVCCExecuteTxn,
+                  txn));
+    }
+  }
+  // RunSerialScheduler();
+}
+
+void TxnProcessor::MVCCExecuteTxn(Txn* txn){
+  //Read all necessary data for this transaction from storage (Note that you should lock the key before each read)
+  // std::cout << "Read all datas" << std::endl << std::flush;  
+    for (set<Key>::iterator it = txn->readset_.begin();
+      it != txn->readset_.end(); ++it) {
+        storage_->Lock(*it);
+        Value result;
+        if (storage_->Read(*it, &result, txn->unique_id_)) {
+          txn->reads_[*it] = result;
+        }
+        storage_->Unlock(*it);
+      }
+
+    for (set<Key>::iterator it = txn->writeset_.begin();
+      it != txn->writeset_.end(); ++it) {
+      storage_->Lock(*it);
+      Value result;
+      if (storage_->Read(*it, &result, txn->unique_id_)){
+        txn->reads_[*it] = result;
+      }
+    }
+
+    // std::cout << "Run" << std::endl << std::flush;  
+    // Execute txn's program logic.
+    txn->Run();
+    completed_txns_.Push(txn);
+
+    // completed_txns_.Push(txn);
+    // std::cout << "Acquire all locks" << std::endl << std::flush;  
+    //Acquire all locks for keys in the write_set_
+    MVCCLockWriteKeys(txn);
+
+    // std::cout << "Check Write" << std::endl << std::flush;  
+    //Call MVCCStorage::CheckWrite method to check all keys in the write_set_
+    if (MVCCCheckWrites(txn)){ //If (each key passed the check)
+      // std::cout << "Apply Write" << std::endl << std::flush;  
+      // Apply the writes
+      ApplyWrites(txn);
+
+      // std::cout << "Release Locks after Applying Writes" << std::endl << std::flush;
+      // Release all locks for keys in the write_set_
+      MVCCUnlockWriteKeys(txn);
+
+      // Hand the txn back to the RunScheduler thread.
+      txn->status_= COMMITTED;
+      txn_results_.Push(txn);
+  
+    }
+    else {
+      // std::cout << "Didnt pass the check" << std::endl << std::flush;
+      // Release all locks for keys in the write_set_
+      MVCCUnlockWriteKeys(txn);
+
+      //cleanup txn
+      txn->reads_.empty();
+      txn->writes_.empty();
+      txn->status_ = INCOMPLETE;
+
+      //Restart txn
+      mutex_.Lock();
+      txn->unique_id_= next_unique_id_;
+      next_unique_id_++;
+      txn_requests_.Push(txn);
+      mutex_.Unlock();
+    }
+
+}
+
+  bool TxnProcessor::MVCCCheckWrites(Txn* txn){
+    bool passed = true;
+    for (set<Key>::iterator it = txn->writeset_.begin();
+      it != txn->writeset_.end(); ++it) {
+        if (!storage_->CheckWrite(*it, txn->unique_id_)){
+          passed = false;
+          break ;
+        }
+    }
+    return passed;
+  }
+
+  void TxnProcessor::MVCCLockWriteKeys(Txn* txn){
+    for (set<Key>::iterator it = txn->writeset_.begin();
+      it != txn->writeset_.end(); ++it) {
+      storage_->Lock(*it);
+    }
+  }
+
+  void TxnProcessor::MVCCUnlockWriteKeys(Txn* txn){
+    for (set<Key>::iterator it = txn->writeset_.begin();
+      it != txn->writeset_.end(); ++it) {
+      storage_->Unlock(*it);
+    }
+  }
+
+  void TxnProcessor::GarbageCollection(){
+
+  }
